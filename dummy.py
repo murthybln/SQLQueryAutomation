0a5b2d3516470950b15c982b7c661ec50f063d68
Narasimha Bandaru (CES Ltd) <narasimhab@Computechcorp.net>
2019-09-16 18:03:46 +0530

    JIRA DATA-0007: removed dummy file

--- dummy.py
+++
@@ -1,9 +0,0 @@
-db_conn = "DRIVER={SQL SERVER};server=192.168.18.36;database=Maverick;uid=narasimha;pwd=Welcome321;"
-db_conn1 = "DRIVER={SQL SERVER};server=192.168.18.36;database=Maverick;uid=narasimha;pwd=Welcome321;"
-
-query_1 = """SELECT * FROM dbo.Employee"""
-
-query_2 = """SELECT * FROM dbo.Employee where Age = 21"""
-
-
-query_3 = """SELECT * FROM dbo.Employee where Age = 21"""
