f7c6b2e547e8704561896338b063ca7ed64aa43e
murthybln <39046240+murthybln@users.noreply.github.com>
2019-09-19 15:06:38 +0530

    commit changes

--- DailySQLQueryAutomation/test_database_scripts.py
+++ DailySQLQueryAutomation/test_database_scripts.py
@@ -20,15 +20,6 @@ def test_query_2():
     assert actual_result == 0, "{0}\n {1}".format(err_msg, result) if actual_result else "Need to contact the Support team to investigate on the issue"
 
 
-def test_query_3():
-    result = database_results.get_db_results(query.db_conn, query.query_3)
-    actual_result = len(result)
-    query_list = []
-    query_list.append(result)
-    err_msg = "Datacheck - query_3 failed with following errors"
-    send_email(query_list, err_msg) if actual_result else ''
-    assert actual_result == 0, "{0}\n {1}".format(err_msg, result) if actual_result else "Need to contact the Support team to investigate on the issue"
-
 def send_email(query_list, subject):
     parsed_data = ft.html_parser(query_list, subject)
     ft.send_email(parsed_data, subject)
